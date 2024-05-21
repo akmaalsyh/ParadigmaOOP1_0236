6e01a2a877e4b3e6677a6c09585518497515d8cc
akmaalsyh <taufansyah50@gmail.com>
2024-05-21 15:20:38 +0700

    memasukkan class di header jantung

---
+++ Komposisi/jantung.h
@@ -0,0 +1,17 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+class jantung 
+{
+public:
+    jantung()
+    {
+        cout << "Jantung dihidupkan\n";
+    }
+
+    ~jantung()
+    {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif
